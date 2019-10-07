2edecc96f299db315499dc3ed776147a48071155
EunJu <qjemfrkwl23@gmail.com>
2019-10-07 14:22:05 +0900

    Front-end 첫 시작

--- haribo-frontend/pages/home.vue.js
+++ haribo-frontend/pages/home.vue.js
@@ -6,11 +6,8 @@ var homeView = Vue.component("Home", {
             <div id="main-overview" class="container">
                 <div class="row">
                     <div class="col-md-6">
-                        <h1>DIGITAL CONTENTS<br>AUCTION</h1>
-                        <h4>블록체인 기반 미술품 경매를 시작해보세요.</h4>
-                    </div>
-                    <div class="col-md-6">
-                      <img width="100%" height="100%" src="./assets/images/badMan.gif">
+                        <h1 class="pixelFont">FIGURE AUCTION</h1>
+                        <h4>블록체인 기반 피규어 경매를 시작해보세요.</h4>
                     </div>
                 </div>
             </div>
