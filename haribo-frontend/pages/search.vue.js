2c17665e93f2cd465c6dfe5214849dc4fda4209a
EunJu <qjemfrkwl23@gmail.com>
2019-10-08 12:00:00 +0900

    artworks and auctions search function

--- haribo-frontend/pages/search.vue.js
+++ haribo-frontend/pages/search.vue.js
@@ -4,6 +4,9 @@ var searchView = Vue.component('searchView', {
         <v-nav></v-nav>
         <div class="container">
         <h3 class="home-margin">" {{ searched }} "으로 검색한 결과입니다.</h3>
+        <div class="col-md-12 text-right">
+            <router-link to="/works/create" class="btn btn-outline-secondary">뒤로가기</router-link>
+        </div>
 
             <div id="my-artwork" class="row">
                 <div class="col-md-12 mt-5">
@@ -28,9 +31,9 @@ var searchView = Vue.component('searchView', {
                 <div class="col-md-12 mt-5">
                     <h4>Auction</h4>
                     <div class="row">
-                        <div class="col-md-3 artwork" v-for="item in auctions" v-if="auctions.length > 0">
+                        <div class="col-md-4 artwork" v-for="item in auctions" v-if="auctions.length > 0">
                             <div class="card">
-                                <div class="card-body">
+                                <div class="card-body text-center">
                                     <img :src="item['작품이미지']">
                                     <h4>{{ item['작품정보']['이름'] }}</h4>
                                     <span class="badge badge-success">{{calculateDate(item['종료일시'])}}</span>
@@ -55,6 +58,44 @@ var searchView = Vue.component('searchView', {
           searched: ""
         }
     },
+    methods: {
+      calculateDate(date) {
+          var now = new Date();
+          var endDate = new Date(date);
+          var diff = Math.floor((Date.parse(endDate) - now) / 1000);
+
+          if(diff < 0) {
+              return "경매 마감";
+          } else {
+              var year = 0
+              var month = 0
+              var day = 0
+              var hour = 0
+              var minute = 0
+              if(Math.floor(diff / 31536000) > 1){
+                year = Math.floor(diff / 31536000)
+                diff = Math.floor(diff % 31536000)
+              }
+              if(Math.floor(diff / 2592000) > 1){
+                month = Math.floor(diff / 2592000)
+                diff = Math.floor(diff % 2592000)
+              }
+              if(Math.floor(diff / 86400) > 1){
+                day = Math.floor(diff / 86400)
+                diff = Math.floor(diff % 86400)
+              }
+              if(Math.floor(diff / 3600) > 1){
+                hour = Math.floor(diff / 3600)
+                diff = Math.floor(diff % 3600)
+              }
+              if( Math.floor(diff / 60) > 1){
+                minute =  Math.floor(diff / 60);
+                diff =  Math.floor(diff % 60);
+              }
+              return  "마감까지 " + month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + diff + "초 남았습니다.";
+          }
+      }
+    },
     mounted: async function(){
       this.searched = this.$route.params.searched;
       var scope = this;
@@ -63,10 +104,26 @@ var searchView = Vue.component('searchView', {
       if(this.searched !==undefined){
       workService.searchWork(this.searched, function(data) {
           scope.artworks = data;
+      });
 
-          auctionService.searchAuction(data, function(res){
+      auctionService.searchAuction(this.searched, function(data){
+        var result = data;
 
-          });
+        function fetchData(start, end){
+            if(start == end) {
+                scope.auctions = result;
+            } else {
+                var id = result[start]['경매작품id'];
+                workService.findById(id, function(work){
+                    result[start]['작품정보'] = work;
+                    fetchData(start+1, end);
+                });
+            }
+        }
+
+        if(result != undefined){
+          fetchData(0, result.length);
+        }
       });
      }
     }
