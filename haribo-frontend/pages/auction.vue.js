b0917138e1186f77fb395daf5ad09d7d575cdefc
rlagpqls123@naver.com <rlagpqls123@naver.com>
2019-10-09 16:25:05 +0900

    Edit Front-End

--- haribo-frontend/pages/auction.vue.js
+++ haribo-frontend/pages/auction.vue.js
@@ -1,5 +1,5 @@
 var auctionView = Vue.component('AuctionView', {
-  template: `
+    template: `
         <div>
             <v-nav></v-nav>
             <v-breadcrumb title="경매 참여하기" description="경매 중인 작품을 보여줍니다."></v-breadcrumb>
@@ -17,10 +17,12 @@ var auctionView = Vue.component('AuctionView', {
                 <div class="col-md-3 auction" v-for="item in pageAuction">
                         <div class="card card-event" style="width: 18rem;">
                             <div class="card-body">
-                              <router-link :to="{ name: 'auction.detail', params: { id: item['id'] }}" class="btn btn-block">  <img  :src="item['작품정보']['작품이미지']"  ></router-link>
+                              <img  :src="item['작품정보']['작품이미지']"  >
                                 <h5 class="card-title">{{ item['작품정보']['이름'] | truncate(10) }}</h5>
-                                <span class="badge badge-navy" v-if="calculateDate(item['종료일시']) == '경매 마감'">{{calculateDate(item['종료일시'])}}</span>
-                                <span class="badge badge-orange" v-if="calculateDate(item['종료일시']) != '경매 마감'">{{calculateDate(item['종료일시'])}}</span>
+                                <p class="badge badge-navy" v-if="calculateDate(item['종료일시']) == '경매 마감'">{{calculateDate(item['종료일시'])}}</p>
+                                <p class="badge badge-orange" v-if="calculateDate(item['종료일시']) != '경매 마감'">{{calculateDate(item['종료일시'])}}</p>
+                                
+                                <router-link :to="{ name: 'auction.detail', params: { id: item['id'] }}" class="btn  btn-block btn-secondary">작품자세히보기</router-link>
                             </div>
                         </div>
                     </div>
@@ -46,138 +48,138 @@ var auctionView = Vue.component('AuctionView', {
             </div>
         </div>
     `,
-  data() {
-    return {
-      auctions: [],
-      pageAuction: [],
-      pageCount: 0,
-      perPage: 8,
-      currentPage: 1,
-      message: "",
-      pages: []
-    }
-  },
-  methods: {
-    search() {
-      var scope = this;
-      if (this.message != "") {
-        auctionService.searchAuction(this.message, function (data) {
-          var result = data;
-          function fetchData(start, end) {
-            if (start == end) {
-              scope.auctions = result;
-              scope.pageCount = Math.ceil(result.length / scope.perPage);
-              scope.movePage(1);
-            } else {
-              var id = result[start]['경매작품id'];
-              workService.findById(id, function (work) {
-                result[start]['작품정보'] = work;
-                fetchData(start + 1, end);
-              });
+    data() {
+        return {
+            auctions: [],
+            pageAuction:[],
+            pageCount: 0,
+            perPage:8,
+            currentPage: 1,
+            message: "",
+            pages:[]
+        }
+    },
+    methods: {
+      search() {
+        var scope = this;
+        if(this.message != ""){
+          auctionService.searchAuction(this.message, function(data) {
+            var result = data;
+            function fetchData(start, end){
+                if(start == end) {
+                    scope.auctions = result;
+                    scope.pageCount = Math.ceil(result.length /scope.perPage);
+                    scope.movePage(1);
+                } else {
+                    var id = result[start]['경매작품id'];
+                    workService.findById(id, function(work){
+                        result[start]['작품정보'] = work;
+                        fetchData(start+1, end);
+                    });
+                }
             }
-          }
 
-          if (result != undefined) {
-            fetchData(0, result.length);
-          }
+            if(result != undefined){
+              fetchData(0, result.length);
+            }
 
-          scope.pageCount = Math.ceil(data.length / scope.perPage);
-          scope.movePage(1);
-        });
-      } else {
-        this.findAll();
-      }
-    },
-    calculateDate(date) {
-      var now = new Date();
-      var endDate = new Date(date);
-      var diff = Math.floor((Date.parse(endDate) - now) / 1000);
-      // 만약 종료일자가 지났다면 "경매 마감"을 표시한다.
-      if (diff < 0) {
-        return "경매 마감";
-      } else {
-        var year = 0
-        var month = 0
-        var day = 0
-        var hour = 0
-        var minute = 0
-        if (Math.floor(diff / 31536000) > 1) {
-          year = Math.floor(diff / 31536000)
-          diff = Math.floor(diff % 31536000)
-        }
-        if (Math.floor(diff / 2592000) > 1) {
-          month = Math.floor(diff / 2592000)
-          diff = Math.floor(diff % 2592000)
-        }
-        if (Math.floor(diff / 86400) > 1) {
-          day = Math.floor(diff / 86400)
-          diff = Math.floor(diff % 86400)
-        }
-        if (Math.floor(diff / 3600) > 1) {
-          hour = Math.floor(diff / 3600)
-          diff = Math.floor(diff % 3600)
-        }
-        if (Math.floor(diff / 60) > 1) {
-          minute = Math.floor(diff / 60);
-          diff = Math.floor(diff % 60);
-        }
-        return "마감: " + month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + diff + "초";
-      }
-    },
-    nextPage() {
-      this.currentPage++;
-      this.movePage(this.currentPage);
-
-    },
-    prevPage() {
-      this.currentPage--;
-      this.movePage(this.currentPage);
-
-    },
-    movePage(p) {
-      this.currentPage = p;
-      var start = (p - 1) * this.perPage
-      var end = this.auctions.length;
-      if (end > start + this.perPage) {
-        end = start + this.perPage
-      }
-      this.pageAuction = [];
-      for (var i = start; i < end; i++) {
-        this.pageAuction.push(this.auctions[i])
-      }
-      this.pages = [];
-      var stPage = this.currentPage - 2 <= 0 ? 1 : this.currentPage - 2;
-      var end = stPage + 5 > this.pageCount ? this.pageCount + 1 : stPage + 5;
-      for (var i = stPage; i < end; i++) {
-        this.pages.push(i);
-      }
-    },
-    findAll() {
-      var scope = this;
-
-      auctionService.findAll(function (data) {
-        var result = data;
-        function fetchData(start, end) {
-          if (start == end) {
-            scope.auctions = result;
-            scope.pageCount = Math.ceil(result.length / scope.perPage);
+            scope.pageCount = Math.ceil(data.length /scope.perPage);
             scope.movePage(1);
-          } else {
-            var id = result[start]['경매작품id'];
-            workService.findById(id, function (work) {
-              result[start]['작품정보'] = work;
-              fetchData(start + 1, end);
-            });
-          }
+          });
+        } else {
+          this.findAll();
         }
+      },
+        calculateDate(date) {
+            var now = new Date();
+            var endDate = new Date(date);
+            var diff = Math.floor((Date.parse(endDate) - now) / 1000);
+            // 만약 종료일자가 지났다면 "경매 마감"을 표시한다.
+            if(diff < 0) {
+                return "경매 마감";
+            } else {
+                var year = 0
+                var month = 0
+                var day = 0
+                var hour = 0
+                var minute = 0
+                if(Math.floor(diff / 31536000) > 1){
+                  year = Math.floor(diff / 31536000)
+                  diff = Math.floor(diff % 31536000)
+                }
+                if(Math.floor(diff / 2592000) > 1){
+                  month = Math.floor(diff / 2592000)
+                  diff = Math.floor(diff % 2592000)
+                }
+                if(Math.floor(diff / 86400) > 1){
+                  day = Math.floor(diff / 86400)
+                  diff = Math.floor(diff % 86400)
+                }
+                if(Math.floor(diff / 3600) > 1){
+                  hour = Math.floor(diff / 3600)
+                  diff = Math.floor(diff % 3600)
+                }
+                if( Math.floor(diff / 60) > 1){
+                  minute =  Math.floor(diff / 60);
+                  diff =  Math.floor(diff % 60);
+                }
+                return  "마감: " + month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + diff+ "초";
+            }
+        },
+        nextPage(){
+          this.currentPage++;
+          this.movePage(this.currentPage);
 
-        if (result != undefined) {
-          fetchData(0, result.length);
+        },
+        prevPage(){
+          this.currentPage--;
+          this.movePage(this.currentPage);
+
+        },
+        movePage(p){
+          this.currentPage = p;
+          var start = (p-1)*this.perPage
+          var end = this.auctions.length;
+          if(end>start+this.perPage){
+            end = start+this.perPage
+          }
+          this.pageAuction = [];
+          for(var i = start; i<end; i++){
+            this.pageAuction.push(this.auctions[i])
+          }
+          this.pages = [];
+          var stPage = this.currentPage-2<=0?1:this.currentPage-2;
+          var end = stPage+5>this.pageCount?this.pageCount+1:stPage+5;
+          for(var i = stPage; i<end; i++){
+            this.pages.push(i);
+          }
+        },
+        findAll() {
+          var scope = this;
+
+          auctionService.findAll(function(data){
+              var result = data;
+              function fetchData(start, end){
+                  if(start == end) {
+                      scope.auctions = result;
+                      scope.pageCount = Math.ceil(result.length /scope.perPage);
+                      scope.movePage(1);
+                  } else {
+                      var id = result[start]['경매작품id'];
+                      workService.findById(id, function(work){
+                          result[start]['작품정보'] = work;
+                          fetchData(start+1, end);
+                      });
+                  }
+              }
+
+              if(result != undefined){
+                fetchData(0, result.length);
+              }
+          });
         }
-      });
+    },
+    mounted: function(){
+        this.findAll();
     }
-  },
-  mounted: function () {
-    this.findAll();
-  }
 });
--- haribo-frontend/pages/home.vue.js
+++ haribo-frontend/pages/home.vue.js
@@ -1,13 +1,13 @@
 var homeView = Vue.component("Home", {
-    props: ["isSigned"],
-    template: `
+  props: ["isSigned"],
+  template: `
         <div>
             <v-nav></v-nav>
             <div id="main-overview" class="container home-margin">
                 <div class="col text-center">
-                  <h1 class="pixelFont">FIGURE AUCTION</h1>
+                  <h1 class="pixelFont"> Photo Copyright Auction</h1>
                   <br>
-                  <h4>블록체인 기반 피규어 경매를 시작해보세요.</h4>
+                  <h4 class="h4-font">블록체인 기반 이미지 저작권 경매를 시작해보세요.</h4>
                   <div class='containers'>
                     <div class='search-box-container'>
                       <button class='submit'>
@@ -26,10 +26,10 @@ var homeView = Vue.component("Home", {
                       <div class="card">
                           <div class="card-body">
                               <img :src="item['작품이미지']">
-                              <h4>{{ item['이름'] }}</h4>
+                              <h5>{{ item['이름'] }}</h5>
                               <p v-if="item['설명'] != null">{{ item["설명"] }}</p>
                               <p v-if="item['설명'] == null">-</p>
-                              <router-link :to="{ name: 'work.detail', params: { id: item['id'] } }" class="btn btn-block btn-style" style="margin-left:0px;">이력보기</router-link>
+                              <router-link :to="{ name: 'work.detail', params: { id: item['id'] } }" class="btn btn-block btn-secondary" style="margin-left:0px;">작품자세히보기</router-link>
                           </div>
                       </div>
                   </div>
@@ -44,125 +44,125 @@ var homeView = Vue.component("Home", {
                           <div class="card-body">
                               <img :src="item['작품정보']['작품이미지']">
                               <h4>{{ item['작품정보']['이름'] }}</h4>
-                              <p>{{ calculateDate(item['종료일시']) }}</p>
-                              <router-link :to="{ name: 'auction.detail', params: { id: item['id'] }}" class="btn btn-block btn-secondary">자세히보기</router-link>
+                                <span class="badge badge-navy" v-if="calculateDate(item['종료일시']) == '경매 마감'">{{calculateDate(item['종료일시'])}}</span>
+                                <span class="badge badge-orange" v-if="calculateDate(item['종료일시']) != '경매 마감'">{{calculateDate(item['종료일시'])}}</span>
+                              <router-link :to="{ name: 'auction.detail', params: { id: item['id'] }}" class="btn btn-block btn-secondary">경매자세히보기</router-link>
                           </div>
                       </div>
                   </div>
               </div>
               </div>
-
             </div>
         </div>
     `,
-    data() {
-        return {
-            sharedState: store.state,
-            message: "",
-            artworks: [],
-            auctions: [],
-            value: ""
-        }
-    },
-    methods: {
-      calculateDate(date) {
-          var now = new Date();
-          var endDate = new Date(date);
-          var diff = Math.floor((Date.parse(endDate) - now) / 1000);
-          // 만약 종료일자가 지났다면 "경매 마감"을 표시한다.
-          if(diff < 0) {
-              return "경매 마감";
-          } else {
-              var year = 0
-              var month = 0
-              var day = 0
-              var hour = 0
-              var minute = 0
-              if(Math.floor(diff / 31536000) > 1){
-                year = Math.floor(diff / 31536000)
-                diff = Math.floor(diff % 31536000)
-              }
-              if(Math.floor(diff / 2592000) > 1){
-                month = Math.floor(diff / 2592000)
-                diff = Math.floor(diff % 2592000)
-              }
-              if(Math.floor(diff / 86400) > 1){
-                day = Math.floor(diff / 86400)
-                diff = Math.floor(diff % 86400)
-              }
-              if(Math.floor(diff / 3600) > 1){
-                hour = Math.floor(diff / 3600)
-                diff = Math.floor(diff % 3600)
-              }
-              if( Math.floor(diff / 60) > 1){
-                minute =  Math.floor(diff / 60);
-                diff =  Math.floor(diff % 60);
-              }
-              return  "마감까지 " + month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + diff + "초 남았습니다.";
-          }
-      },
-      Enter() {
-        this.$router.push({ name: 'search', params: { searched: this.value }})
-      }
-    },
-    mounted: function() {
-      var scope = this;
-
-      workService.findAll(function(data){
-          scope.artworks.push(data[0]);
-          scope.artworks.push(data[1]);
-          scope.artworks.push(data[2]);
-      });
-
-      auctionService.homeAuction(function(data){
-          var result = data;
-
-          function fetchData(start, end){
-              if(start == end) {
-                scope.auctions.push(result[0]);
-                scope.auctions.push(result[1]);
-                scope.auctions.push(result[2]);
-              } else {
-                  var id = result[start]['경매작품id'];
-                  workService.findById(id, function(work){
-                      result[start]['작품정보'] = work;
-                      fetchData(start+1, end);
-                  });
-              }
-          }
-
-          if(result != undefined){
-            fetchData(0, result.length);
-          }
-      });
-
-      $.fn.toggleState = function(b) {
-      	$(this).stop().animate({
-      		width: b ? "450px" : "50px"
-      	}, 600, "easeOutElastic" );
-      }
-
-      $(document).ready(function() {
-      	var container = $(".container");
-      	var boxContainer = $(".search-box-container");
-      	var submit = $(".submit");
-      	var searchBox = $(".search-box");
-      	var response = $(".response");
-      	var isOpen = false;
-      	submit.on("mousedown", function(e) {
-      		e.preventDefault();
-      		boxContainer.toggleState(!isOpen);
-      		isOpen = !isOpen;
-      		if(!isOpen) {
-      			handleRequest();
-      		} else {
-      			searchBox.focus();
-      		}
-      	});
-      	searchBox.blur(function() {
-      		boxContainer.toggleState(false);
-      		isOpen = false;
-      	});
-      });
+  data() {
+    return {
+      sharedState: store.state,
+      message: "",
+      artworks: [],
+      auctions: [],
+      value: ""
     }
+  },
+  methods: {
+    calculateDate(date) {
+      var now = new Date();
+      var endDate = new Date(date);
+      var diff = Math.floor((Date.parse(endDate) - now) / 1000);
+      // 만약 종료일자가 지났다면 "경매 마감"을 표시한다.
+      if (diff < 0) {
+        return "경매 마감";
+      } else {
+        var year = 0
+        var month = 0
+        var day = 0
+        var hour = 0
+        var minute = 0
+        if (Math.floor(diff / 31536000) > 1) {
+          year = Math.floor(diff / 31536000)
+          diff = Math.floor(diff % 31536000)
+        }
+        if (Math.floor(diff / 2592000) > 1) {
+          month = Math.floor(diff / 2592000)
+          diff = Math.floor(diff % 2592000)
+        }
+        if (Math.floor(diff / 86400) > 1) {
+          day = Math.floor(diff / 86400)
+          diff = Math.floor(diff % 86400)
+        }
+        if (Math.floor(diff / 3600) > 1) {
+          hour = Math.floor(diff / 3600)
+          diff = Math.floor(diff % 3600)
+        }
+        if (Math.floor(diff / 60) > 1) {
+          minute = Math.floor(diff / 60);
+          diff = Math.floor(diff % 60);
+        }
+        return "마감: " + month + "월 " + day + "일 " + hour + "시 " + minute + "분 " + diff + "초";
+      }
+    },
+    Enter() {
+      this.$router.push({ name: 'search', params: { searched: this.value } })
+    }
+  },
+  mounted: function () {
+    var scope = this;
+
+    workService.findAll(function (data) {
+      scope.artworks.push(data[0]);
+      scope.artworks.push(data[1]);
+      scope.artworks.push(data[2]);
+    });
+
+    auctionService.homeAuction(function (data) {
+      var result = data;
+
+      function fetchData(start, end) {
+        if (start == end) {
+          scope.auctions.push(result[0]);
+          scope.auctions.push(result[1]);
+          scope.auctions.push(result[2]);
+        } else {
+          var id = result[start]['경매작품id'];
+          workService.findById(id, function (work) {
+            result[start]['작품정보'] = work;
+            fetchData(start + 1, end);
+          });
+        }
+      }
+
+      if (result != undefined) {
+        fetchData(0, result.length);
+      }
+    });
+
+    $.fn.toggleState = function (b) {
+      $(this).stop().animate({
+        width: b ? "650px" : "50px"
+      }, 600, "easeOutElastic");
+    }
+
+    $(document).ready(function () {
+      var container = $(".container");
+      var boxContainer = $(".search-box-container");
+      var submit = $(".submit");
+      var searchBox = $(".search-box");
+      var response = $(".response");
+      var isOpen = false;
+      submit.on("mousedown", function (e) {
+        e.preventDefault();
+        boxContainer.toggleState(!isOpen);
+        isOpen = !isOpen;
+        if (!isOpen) {
+          handleRequest();
+        } else {
+          searchBox.focus();
+        }
+      });
+      searchBox.blur(function () {
+        boxContainer.toggleState(false);
+        isOpen = false;
+      });
+    });
+  }
 })
