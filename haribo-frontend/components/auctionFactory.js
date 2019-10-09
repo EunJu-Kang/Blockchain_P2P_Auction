1a7979a7ce7b9c1d87b13dfe36a29920df8884ab
rlagpqls123@naver.com <rlagpqls123@naver.com>
2019-10-09 19:41:07 +0900

    cleancode

--- haribo-frontend/components/auctionFactory.js
+++ haribo-frontend/components/auctionFactory.js
@@ -1,26 +1,19 @@
-// Web3 Object 생성
 function createWeb3(){
     var web3 = new Web3(new Web3.providers.HttpProvider(BLOCKCHAIN_URL));
     return web3;
 }
 
-// AuctionFactory 컨트랙트 Object 생성
+
 function createFactoryContract(web3){
     var auctionContract = new web3.eth.Contract(AUCTION_FACTORY_CONTRACT_ABI, AUCTION_CONTRACT_ADDRESS);
     return auctionContract;
 }
 
-// Auction Object 컨트랙트 생성
 function createAuctionContract(web3, contractAddress){
     var auctionContract = new web3.eth.Contract(AUCTION_CONTRACT_ABI, contractAddress);
     return auctionContract;
 }
 
-/**
- * TODO [경매 생성]
- * AuctionFactory의 createAuction 함수를 호출하여 경매를 생성합니다.
- * 경매 생성 시, (작품id, 최소입찰가, 경매시작시간, 경매종료시간)을 반드시 지정해야합니다.
- *  */
  function createAuction(options, walletAddress, privateKey, onConfirm){
    var web3 = createWeb3();
    var contract = createFactoryContract(web3);
@@ -42,11 +35,6 @@ function createAuctionContract(web3, contractAddress){
    })
  }
 
-/**
- * TODO [입찰]
- * 해당 컨트랙트 주소의 bid함수를 호출하여 입찰합니다.
- * 경매 컨트랙트 주소: options.contractAddress
- *  */
  function auction_bid(options, onConfirm){
    var web3 = createWeb3();
    var contract = createAuctionContract(web3, options.contractAddress);
@@ -71,11 +59,6 @@ function createAuctionContract(web3, contractAddress){
    })
  }
 
-/**
- * TODO [경매 종료]
- * 해당 컨트랙트 주소의 endAuction함수를 호출하여 경매를 종료합니다.
- * 경매 컨트랙트 주소: options.contractAddress
- *  */
 function auction_close(options, onConfirm){
   var web3 = createWeb3();
   var contract = createAuctionContract(web3, options.contractAddress);
@@ -98,11 +81,6 @@ function auction_close(options, onConfirm){
   })
 }
 
-/**
- * TODO [경매 취소]
- * 해당 컨트랙트 주소의 cancelAuction함수를 호출하여 경매를 종료합니다.
- * 경매 컨트랙트 주소: options.contractAddress
- *  */
 function auction_cancel(options, onConfirm){
   var web3 = createWeb3();
   var contract = createAuctionContract(web3, options.contractAddress);
